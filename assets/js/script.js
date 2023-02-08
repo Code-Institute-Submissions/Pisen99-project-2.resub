9d7df104ac4e9ce3b8ef3004826c8a1f064abc05
Pisen99 <patricia.andersson99@hotmail.com>
2023-02-08 15:27:37 +0000

    Fixed: blank lines in js & a second comment added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
-// Rules button
+// Pop-up feature for Rules button
 const rulesPopUp = document.getElementById('rules-container');
+
 
 function popupContainer() {
     rulesPopUp.style.visibility = 'visible';
 }
 
+// closing for Rules button
 function popupContainerClose() {
     rulesPopUp.style.visibility = 'hidden';
 }
