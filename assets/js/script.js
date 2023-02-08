b810925c69e01a2e740b22773736fde8bf2c78b0
Pisen99 <patricia.andersson99@hotmail.com>
2023-02-08 16:56:07 +0000

    Fixed: removed id's on game icons section in html. Added selections with img's in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,37 @@
 // Created basic structure to the game with https://www.youtube.com/watch?v=1yS-JV4fWqY&t=27s so I could easier understand how to write my own code and how it works.
 const selectionButtons = document.querySelectorAll("[data-selection]");
+const SELECTIONS = [
+    {
+        name: "rock",
+        image: "./assets/images/rock.png",
+        beats: "scissor"
+    },
+    {
+        name: "paper",
+        image: "./assets/images/paper.png",
+        beats: "rock"
+    },
+    {
+        name: "scissor",
+        image: "./assets/images/scissor.png",
+        beats: "paper"
+    }
+];
 
 
 // Finds the name of the game-icons selections & makes the selection on a click
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener("click", e => {
         const selectionName = selectionButton.dataset.selection;
-        makeSelection(selectionName);
+        const selection = SELECTIONS.find(selection => selection.name === selectionName);
+        makeSelection(selection);
     });
 });
 
 
 function makeSelection(selection) {
-    console.log(selection)
+    console.log(selection);
 }
-
 
 // Pop-up feature for Rules button
 const rulesPopUp = document.getElementById("rules-container");
