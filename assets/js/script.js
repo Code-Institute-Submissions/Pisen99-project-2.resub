81daa1a5e3293b8ecf5f15170deaeba985065de4
Pisen99 <patricia.andersson99@hotmail.com>
2023-02-08 16:19:07 +0000

    Feat: js, added a function to show what game-icon player clicks on

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,30 @@
+// Created basic structure to the game with https://www.youtube.com/watch?v=1yS-JV4fWqY&t=27s so I could easier understand how to write my own code and how it works.
+const selectionButtons = document.querySelectorAll("[data-selection]");
+
+
+// Finds the name of the game-icons selections & makes the selection on a click
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener("click", e => {
+        const selectionName = selectionButton.dataset.selection;
+        makeSelection(selectionName);
+    });
+});
+
+
+function makeSelection(selection) {
+    console.log(selection)
+}
+
+
 // Pop-up feature for Rules button
-const rulesPopUp = document.getElementById('rules-container');
+const rulesPopUp = document.getElementById("rules-container");
 
 
 function popupContainer() {
-    rulesPopUp.style.visibility = 'visible';
+    rulesPopUp.style.visibility = "visible";
 }
 
 // closing for Rules button
 function popupContainerClose() {
-    rulesPopUp.style.visibility = 'hidden';
+    rulesPopUp.style.visibility = "hidden";
 }
